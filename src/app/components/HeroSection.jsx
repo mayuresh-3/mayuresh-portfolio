493f63801659e88b571c9ea68975c22fcdaf1bcf
mayuresh-tech9 <mayuresh.gramopadhye@tech9.com>
2024-05-30 15:35:53 +0530

    Changes for css and env

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -15,7 +15,7 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
         >
-          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl lg:leading-normal font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-200 to-secondary-400">
               Mayuresh Gramopadhye
             </span>
