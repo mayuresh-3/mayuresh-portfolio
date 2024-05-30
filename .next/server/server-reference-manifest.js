c90e33f106da2af64ed11ccbf09cbbbada5f06ca
mayuresh-tech9 <mayuresh.gramopadhye@tech9.com>
2024-05-30 14:37:32 +0530

    Initial template commit

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"LxPwNn2HTRfP7RgwCDvmj6at1xjlgnYNO+xrtboHNuc=\"\n}"
---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,87 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>PHP</li>
+        <li>Laravel</li>
+        <li>ReactJS</li>
+        <li>Node.js</li>
+        <li>Express</li>
+        <li>SQL</li>
+        <li>ElectronJS</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>MCA, Pune University</li>
+      </ul>
+    ),
+  }
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/images/about-image.png" width={500} height={500} />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+            I am a full stack web developer with over 10 years,
+            I am a Senior Software Engineer at Tech9, a leading provider of innovative and scalable solutions for clients across various industries.
+            I have a strong expertise in web and cloud technologies, such as PHP, ReactJS, SQL, AWS, Laravel, and Go
+            and I have completed multiple certifications to enhance my knowledge and proficiency.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            {/*<TabButton*/}
+            {/*  selectTab={() => handleTabChange("certifications")}*/}
+            {/*  active={tab === "certifications"}*/}
+            {/*>*/}
+            {/*  {" "}*/}
+            {/*  Certifications{" "}*/}
+            {/*</TabButton>*/}
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
